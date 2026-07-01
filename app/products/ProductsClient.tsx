e082e5b5c771d085c7f053fde090c73e349ef72b
Akbar Ali <akbarali1512141@gmail.com>
2026-07-01 23:23:38 +0500

    fix products suspense issue

---
+++ app/products/ProductsClient.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from 'react'
+import ProductsClient from './ProductsClient'
+
+export const dynamic = 'force-dynamic'
+
+export default function ProductsPage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          Loading products...
+        </div>
+      }
+    >
+      <ProductsClient />
+    </Suspense>
+  )
+}
